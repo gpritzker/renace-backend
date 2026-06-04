018f3f540d6ba6a6dd2c3f73a19bf23f50fb17f0
Gonzalo Pritzker <gonzalo.pritzker@avenida.com>
2026-06-04 17:48:11 -0300

    cambios en redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,9 @@
 # config/initializers/sidekiq.rb
-require "uri"
 require "openssl"
 
-redis_uri = URI.parse(ENV.fetch("REDIS_URL"))
-
 redis_config = {
-  host: redis_uri.host,
-  port: redis_uri.port,
-  username: redis_uri.user,
-  password: redis_uri.password,
-  ssl: redis_uri.scheme == "rediss",
+  url: ENV.fetch("REDIS_URL"),
+  ssl: true,
   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 }
 
