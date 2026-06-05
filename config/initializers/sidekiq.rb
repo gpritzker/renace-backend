02fbe9545efe58829796aea3225a72c2d4dccfcc
Gonzalo Pritzker <gonzalo.pritzker@avenida.com>
2026-06-05 08:33:52 -0300

    cambio sidekiq v1

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,16 @@
 # config/initializers/sidekiq.rb
 require "openssl"
 
+redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/0").strip
+
 redis_config = {
-  url: ENV.fetch("REDIS_URL"),
-  ssl: true,
-  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  url: redis_url
 }
+
+if redis_url.start_with?("rediss://")
+  redis_config[:ssl] = true
+  redis_config[:ssl_params] = { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+end
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
