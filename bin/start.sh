0ab4acf9e92494a4ad9ae2da456e510321df380d
Gonzalo Pritzker <gonzalo.pritzker@avenida.com>
2026-06-09 09:29:52 -0300

    renicio de sidekiq

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
-bundle exec sidekiq &
-bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
+
+# Reinicia Sidekiq automáticamente si muere
+(while true; do
+  echo "[start.sh] Iniciando Sidekiq..."
+  bundle exec sidekiq || true
+  echo "[start.sh] Sidekiq terminó, reiniciando en 5s..."
+  sleep 5
+done) &
+
+bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RAILS_ENV:-production}
